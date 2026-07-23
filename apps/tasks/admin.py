738233a071836e14a71407d03edcafec78fdd555
AntonTrofimov2025 <xerurganton@gmail.com>
2026-07-23 22:13:39 +0200

    HW11 is DONE!

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Task, SubTask
+from .models import Category, Task, SubTask, Statuses
 
 
 @admin.register(Category)
@@ -8,12 +8,24 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = ('-name',)
 
 
+class SubTaskInline(admin.StackedInline):
+    model = SubTask
+    extra = 1
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at', 'description', 'status', 'deadline')
+    list_display = ('short_title', 'created_at', 'updated_at', 'description', 'status', 'deadline')
     list_editable = ('description', 'status', 'deadline')
     ordering = ('-title',)
     search_fields = ('title',)
+    inlines = [SubTaskInline]
+
+    @admin.display(description='Description')
+    def short_title(self, task):
+        if task.title and len(task.title) > 10:
+            return f'{task.title[:10]}...'
+        return task.title
 
 
 @admin.register(SubTask)
@@ -23,6 +35,12 @@ class SubTaskAdmin(admin.ModelAdmin):
     ordering = ('-title',)
     search_fields = ('title',)
 
+    @admin.action(description="Set SubTask's status to DONE")
+    def change_status_to_done(self, request, subtasks):
+        # for subtask in subtasks:
+        #     subtask.status = Statuses.DONE
+        #     subtask.save()
+        subtasks.update(status=Statuses.DONE)
 
-
+    actions = [change_status_to_done]
 
